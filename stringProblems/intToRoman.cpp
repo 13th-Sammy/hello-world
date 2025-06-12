c58491900b823d62a34269cd52d218b525392c55
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-06-12 14:27:13 +0530

    Update

--- stringProblems/intToRoman.cpp
+++ stringProblems/intToRoman.cpp
@@ -10,6 +10,10 @@ int main()
     int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
     string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     string romanNum="";
+    // Subtract value if num is bigger,
+    // for every value subtraction add
+    // corresponding symbol. All values 
+    // subtracted=all symbols added.
     for(int i=0; i<13; i++)
     {
         while(num>=values[i])
